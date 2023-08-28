65934c287c8a262c2917d812e3a4b4ef54911397
thelordzeus <arnav55125@gmail.com>
2023-08-28 19:16:22 +0530

    feat: added google auth

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,15 +1,23 @@
-import { getProviders } from "next-auth/react";
-export default function LoginPage({ providers }) {
+"use client";
+import { getProviders, signIn } from "next-auth/react";
+export default async function LoginPage({}) {
+  const providers = await getProviders();
+
   return (
     <div className="flex items-center justify-center h-screen ">
-      login page here
+      {Object.values(providers).map((provider) => (
+        <div>
+          <button
+            onClick={async () => {
+              await signIn(provider.id);
+            }}
+            className=" bg-twitterWhite pl-3 pr-5 py-2 text-black rounded-full flex items-center space-x-2"
+          >
+            <img src="/googleImg.png" alt="Google Icon " className="h-8" />
+            <span>Sign in with {provider.name}</span>
+          </button>
+        </div>
+      ))}
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const providers = await getProviders();
-  return {
-    props: { providers },
-  };
 }
